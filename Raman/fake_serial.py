d31f13654a2d48c63cacbb8ed69aca0feb687d30
Zhi Zi <x@zzi.io>
2018-07-05 02:45:27 +0800

    fix export bug

--- Raman/fake_serial.py
+++ Raman/fake_serial.py
@@ -22,8 +22,8 @@ class FakeSerial():
     def read(self, bytes_length):
         self.data_length = bytes_length // 2
         self.fmt = '<{data_length}H'.format(data_length=self.data_length)
-        self.fake_data = np.random.randint(1, 3000, self.data_length)
-        # self.fake_data = random_walk(15000, self.data_length, 3)
+        # self.fake_data = np.random.randint(1, 3000, self.data_length)
+        self.fake_data = random_walk(15000, self.data_length, 3)
         return struct.pack(self.fmt, *self.fake_data)
 
     def inWaiting(self):
--- Raman/static/js/export_to_json.js
+++ Raman/static/js/export_to_json.js
@@ -1,6 +1,31 @@
+function parseJSONToPyJSON(jsonData) {
+    if(jsonData.length == 0) {
+        return '';
+    }
+    
+    let pyJson = {};
+
+    pyJson['rawdata_x'] = jsonData['rawdata_x'];
+
+    var intensitiesObj = jsonData['intensities']
+    pyJson['intensities'] = [];
+    for (const i in intensitiesObj) {
+        pyJson['intensities'].push(intensitiesObj[i])
+    }
+
+    var intensitiesObj = jsonData['raman_spec_x']
+    pyJson['raman_spec_x'] = [];
+    for (const i in intensitiesObj) {
+        pyJson['raman_spec_x'].push(intensitiesObj[i])
+    }
+
+    return pyJson;
+}
+
 
 function exportToJsonFile(jsonData) {
-    let dataStr = JSON.stringify(jsonData);
+    let pyJson = parseJSONToPyJSON(jsonData);
+    let dataStr = JSON.stringify(pyJson);
     let dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(dataStr);
     
     let exportFileDefaultName = 'data.json';
